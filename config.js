9ba89c869ffbccb2575e52c5401103c6503ba4f2
NethsaraSandaruwan0 <knethuva@gmail.com>
2025-02-08 10:09:51 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,12 +5,12 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Past Your Session Id 🍃",
+SESSION_ID: process.env.SESSION_ID || "𝐐𝐔𝐄𝐄𝐍-𝐑𝐀𝐒𝐇𝐔-𝐌𝐃=084XRAwA#BSKRTRWb952O1jP_uM-remQlsXRfWTzzv17d21qwO4E",
 //උඩ ☝️ තියෙන Past Sesion Id කියන එක අයින් කරලා ඔයාගෙ Sesion Id එක Past කරන්න 😁
-MONGODB: process.env.MONGODB || "Past Your MongoDB Url 🍃",
+MONGODB: process.env.MONGODB || "mongodb://mongo:pbMHUjXERYKyIRnkbNgtNtWmZlVZOpwI@roundhouse.proxy.rlwy.net:28139",
 //උඩ ☝️ තියෙන Past MongoDB Url කියන එක අයින් කරලා ඔයාගෙ MongoDB Url එක Past කරන්න 😁
-ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/BsjkCDP/9555.jpg",
-BOT_NAME: process.env.BOT_NAME || "QUEEN-RASHU-MD",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/R75SQD5/4582.jpg",
+BOT_NAME: process.env.BOT_NAME || "KING-NETHUVA-MD",
 LANG: process.env.BOT_LANG || 'EN' ,
 OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39",
 AUTO_TYPING: process.env.AUTO_TYPING || "false",
